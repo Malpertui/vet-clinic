8b72be5fd1920dceb6c895dabf5ce8c1dad80d1c
Malpertui <malpertui@gmail.com>
2024-01-21 04:56:52 +0200

    req-08_Add registration date for Pet

--- src/main/java/com/magicvet/Sandbox.java
+++ src/main/java/com/magicvet/Sandbox.java
@@ -4,20 +4,6 @@ import main.java.com.magicvet.model.Cat;
 
 public class Sandbox {
     public static void main(String[] args) {
-        /*Dog[] dogs = {
-                new Dog(Dog.M),
-                new Dog(Dog.S),
-                new Dog(Dog.XL),
-                new Dog(Dog.XL),
-                new Dog(Dog.XS),
-                new Dog(Dog.S)
-
-        };
-        Arrays.sort(dogs, new DogSizeComparator());
-
-        for (Dog dog : dogs) {
-            System.out.println(dog.getSize());
-        }*/
 
         System.out.println(new Cat());
 
